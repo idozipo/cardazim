395af727361ace81e6116bb14d582d9768a843d7
idozipo <idozipori08@gmail.com>
2026-09-15 16:18:40 +0300

    implemented card serialization

--- cardazim/crypt_image.py
+++ cardazim/crypt_image.py
@@ -10,6 +10,10 @@ from Crypto.Cipher import AES
 
 NONCE = b'arazim'
 IMAGE_MODE = 'RGB'
+IMAGE_SIZE_FORMAT_STR = "<II"
+
+HASH_KEY_SIZE = 32
+ZERO_HASH = b'\x00' * HASH_KEY_SIZE
 
 class CryptImage:
     def __init__(self, image: Image.Image, key_hash: bytes | None):
@@ -73,7 +77,34 @@ class CryptImage:
     def serialize(self) -> bytes:
         """ Serializes the image into a byte format (including metadata) """
         return (
-            struct.pack("II", self.image.size[1], self.image.size[0]) + \
+            struct.pack(IMAGE_SIZE_FORMAT_STR, self.image.size[1], self.image.size[0]) + \
             self.image.tobytes() + \
-            self.key_hash if self.key_hash else b''
+            self.key_hash if self.key_hash else ZERO_HASH
         )
+
+    @classmethod
+    def deserialize(cls, data: bytes, offset: int) -> tuple[CryptImage, int]:
+        """
+        Deserializes the CryptImage from the bytes format.
+        """
+
+        # Extract the size metadata
+        height, width = struct.unpack_from(IMAGE_SIZE_FORMAT_STR, data, offset)
+        offset += struct.calcsize(IMAGE_SIZE_FORMAT_STR)
+
+        # Calculate the amount of bytes in the image
+        data_amount = 3 * (width * height)
+
+        # Recreate image from metadata + bytes
+        # There are W * H pixels and each pixel is represented by 3 bytes
+        image = Image.frombytes(IMAGE_MODE, (width, height), data[offset:offset + data_amount])
+        offset += data_amount
+
+        # Extract the hash (all zeroes marks the empty hash)
+        key_hash = data[offset:offset + HASH_KEY_SIZE]
+        offset += HASH_KEY_SIZE
+
+        return (cls(
+            image,
+            key_hash if key_hash != ZERO_HASH else None
+        ), offset)
